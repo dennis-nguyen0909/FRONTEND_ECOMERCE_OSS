7b41680408f6172de5d9106df4a45f62848b8f17
dennis <dennis.nguyen0909@gmail.com>
2024-12-18 12:47:41 +0700

    call api get-detail-user

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -11,3 +11,14 @@ export const loginUser = async (data) => {
   const res = await axios.post(`${process.env.REACT_APP_API}user/login`, data);
   return res.data;
 };
+export const getDetailUser = async (id, access_token) => {
+  const res = await axiosJWT.get(
+    `${process.env.REACT_APP_API}user/get-details/${id}`,
+    {
+      headers: {
+        token: `Bearer ${access_token}`,
+      },
+    }
+  );
+  return res.data;
+};
