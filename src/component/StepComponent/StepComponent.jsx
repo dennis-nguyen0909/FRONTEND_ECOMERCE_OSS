a3f27c545a8763443167e7e5992fe41de33cf191
quocanh <ctmdev03@gmail.com>
2024-12-19 23:34:22 +0700

    addcomponentstep

---
+++ src/component/StepComponent/StepComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Steps } from 'antd'
+export const StepComponent = ({ current = 0, items = [] }) => {
+    const { Step } = Steps
+    return (
+        <Steps current={current}>
+            {items.map((item) => {
+                return (
+                    < Step key={item.title} title={item.title} description={item.description} />
+                )
+            })}
+
+        </Steps>
+    )
+}
