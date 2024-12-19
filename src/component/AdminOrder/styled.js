372f8c4cf5971ea2432283e5f81187785f1252ed
quocanh <ctmdev03@gmail.com>
2024-12-19 22:16:04 +0700

    adminorder

---
+++ src/component/AdminOrder/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WrapperDivInfoProduct = styled.div`
+    color:black;
+    font-size:20px;
+    .p {
+        font-size: 14px;
+        color:red;
+    }
+
+`
