792af8cae7b6d91f6bb831a7452d8aaf44101aab
Phong <phongchau346@gmail.com>
2024-12-19 21:54:00 +0700

    Create-LoadingComponent

--- src/pages/SearchProduct/SearchProduct.jsx
+++ src/pages/SearchProduct/SearchProduct.jsx
@@ -30,7 +30,37 @@ export const SearchProduct = () => {
             <div className='body' style={{ width: '100%', backgroundColor: "#fff" }}>
                 <div id="container" style={{ height: 'fit-content' }}>
                     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '20px 0', fontSize: '30px', }}>Sản Phẩm </div>
-                    
+                    <LoadingComponent isLoading={isLoading}>
+                        <WrapperProduct>
+                            {products?.data && products?.data?.length > 0 ? (
+                                products?.data.map((product) => (
+                                    < CardComponent
+                                        id={product._id}
+                                        key={product._id}
+                                        countInStock={product.countInStock}
+                                        description={product.description}
+                                        image={product.image}
+                                        name={product.name}
+                                        price={product.price}
+                                        rating={product.rating}
+                                        type={product.type}
+                                        discount={product.discount}
+                                        selled={product.selled}
+                                    />
+                                ))
+                            ) : (
+                                <p>Không tìm thấy sản phẩm</p>
+                            )}
+                        </WrapperProduct>
+                    </LoadingComponent>
+                    <div style={{ width: '100%', display: 'flex', justifyContent: 'center', margin: '15px 0' }}>
+                        <WrapperButtonMore type={'outline'} textButton={'Xem thêm'} styleButton={{
+                            border: '1px solid #ccc', color: 'black', width: '240px',
+                            height: '38px', borderRadius: '4px',
+                        }}
+                            styleTextButton={{ fontWeight: '500' }} onClick={handleLoadMore}
+                            disabled={products?.total === products?.data?.length || products?.totalPage === 1} />
+                    </div>
                     {/* <NavbarComponent /> */}
                 </div>
             </div>
