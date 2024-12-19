47fc0037b7a63603c42e23222436b86d0188d337
Phong <phongchau346@gmail.com>
2024-12-19 22:33:36 +0700

    Create-Routes

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+import { ProfileUserPage } from "../pages/ProfileUserPage/ProfileUserPage";
+export const routes = [
+   
+    {
+        path: '/profile-user',
+        page: ProfileUserPage,
+        isShowHeader: true,
+        isShowFooter: false,
+    },
+   
+]
