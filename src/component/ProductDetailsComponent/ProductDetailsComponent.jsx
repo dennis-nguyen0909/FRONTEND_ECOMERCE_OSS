dbd8f47fb671b5bc16b2b6301888d9fd70655452
dennis <dennis.nguyen0909@gmail.com>
2024-12-19 22:40:55 +0700

    gia lap bug

--- src/component/ProductDetailsComponent/ProductDetailsComponent.jsx
+++ src/component/ProductDetailsComponent/ProductDetailsComponent.jsx
@@ -50,8 +50,6 @@ export const ProductDetailsComponent = ({ idProduct }) => {
         } else {
             if (productDetail?.countInStock <= 0 || numProduct > productDetail?.countInStock) {
                 message.error("Sản phẩm này đã hết vui lòng chọn sản phẩm khác !")
-            } else if (!selectedSize) {
-                message.error("Vui lòng chọn size !")
             }
             else {
                 if (numProduct > 0 && productDetail) {
