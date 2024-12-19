9250aff3cbf4f920f7df28a8edfef1e0ccecf152
Phong <phongchau346@gmail.com>
2024-12-19 22:14:34 +0700

    Create-Routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+
+import { SupportPage } from "../pages/SupportPage/SupportPage";
+export const routes = [
+    
+    {
+        path: '/support',
+        page: SupportPage,
+        isShowHeader: true,
+        isShowFooter: false,
+    },
+    
+]
