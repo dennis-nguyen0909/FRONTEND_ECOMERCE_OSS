f85621ba250aff015296806e60fab46b6ccf9c04
quocanh <ctmdev03@gmail.com>
2024-12-19 23:02:38 +0700

    createroutesmyorderpage

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
 import { HomePage } from "../pages/HomePage/HomePage";
 import { ProductPage } from "../pages/ProductPage/ProductPage";
 import { SearchProduct } from "../pages/SearchProduct/SearchProduct";
+import { MyOrderPage } from "../pages/MyOrderPage/MyOrderPage";
 
 export const routes = [
   {
@@ -53,4 +54,10 @@ export const routes = [
     isShowHeader: true,
     isShowFooter: false,
   },
+  {
+    path: '/my-order',
+    page: MyOrderPage,
+    isShowHeader: true,
+    isShowFooter: false,
+},
 ];
